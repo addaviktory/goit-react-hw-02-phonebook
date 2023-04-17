37b2e91e807d6383869341f45a4b597f15ea446d
addaviktory <112877379+addaviktory@users.noreply.github.com>
2023-04-17 16:50:56 +0300

    homeWork

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
-export const App = () => {
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import { Contacts } from './Contacts/Contacts';
+import ContactsList from './ContactsList/ContactsList';
+import ContactsFilter from './ContactsFilter/ContactsFilter';
+
+function App() {
+  const [contacts, setContacts] = useState([
+    { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+    { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+    { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+    { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+
+  const formSubmitHandler = data => {
+    setContacts(prevContacts => [...prevContacts, { ...data, id: nanoid() }]);
+  };
+
+  const onFilter = e => {
+    setFilter(e.target.value);
+  };
+
+  const getVisibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
+  };
+
+  const visibleContacts = getVisibleContacts();
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="container">
+      <h1>PhoneBook</h1>
+      <Contacts onSubmit={formSubmitHandler} />
+
+      <div>
+        <h2>Contacts</h2>
+        <ContactsFilter value={filter} onFilter={onFilter} />
+        <ContactsList contacts={visibleContacts} onDeleteContact={deleteContact} />
+      </div>
     </div>
   );
-};
+}
+
+export default App;
